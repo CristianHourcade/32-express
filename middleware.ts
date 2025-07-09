84d1e1df117d77e425b049d78c6f4e9340c6d0bf
Sandra Qwe <hourcadecristian@gmail.com>
2025-07-09 17:33:31 -0300

    Deploy api

--- middleware.ts
+++ middleware.ts
@@ -24,14 +24,9 @@ export function middleware(request: NextRequest) {
 // Configurar el middleware para ejecutarse en todas las rutas
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public (public files)
-     */
-    "/((?!_next/static|_next/image|favicon.ico|public).*)",
+    // Aplica a todas las rutas excepto /_next/*, /favicon.ico, /public/* y /api/*
+    "/((?!_next/static|_next/image|favicon.ico|public|api).*)",
   ],
 }
+
 
