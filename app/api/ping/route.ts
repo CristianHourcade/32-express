d554707f5e2041aedb322c42c7b3ea0cb25a2c01
Sandra Qwe <hourcadecristian@gmail.com>
2025-07-10 08:01:48 -0300

    Test

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,5 +1,22 @@
 import { NextResponse } from "next/server";
 
+export async function POST(req: any) {
+    console.log("[API] POST recibido en /api/facturar");
+
+    const body = await req.json();
+
+    const res = await fetch("https://www.tusfacturas.app/app/api/v2/facturacion/nuevo", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+    return NextResponse.json(data);
+}
+
 export function GET() {
     return NextResponse.json({ pong: true });
 }
